cd3928f18c63b3a2526f7c17df2cc38641c3ab83
carlos676767 <carlitobr730@gmail.com>
2024-04-09 16:48:18 -0700

    encurtando de links ok.

--- link/src/main.ts
+++ link/src/main.ts
@@ -1,42 +1,50 @@
-
 const botao = document.querySelector(".enviar") as HTMLButtonElement;
 
-
 const mensagemDminioCom = () => {
-  alert("Use o .com")
-}
+  alert("Use o .com");
+};
 
 const exibirUrl = (data: string) => {
   const p = document.querySelector("p") as HTMLParagraphElement;
-  p.textContent = data
-}
+  p.textContent = data;
+};
 
 const copiarUrl = (data: any) => {
-  navigator.clipboard.writeText(data)
-}
+  navigator.clipboard.writeText(data);
+};
 
 const mensagemVazioInput = () => {
-  alert("O input esta vazio")
+  alert("O input esta vazio");
+};
+
+const url = document.querySelector(".valor") as HTMLInputElement;
+
+const exibirResultados = (link: string) => {
+  const exibirLink = document.getElementById("p") as HTMLElement
+  exibirLink.innerHTML = link
 }
 
-
-const encurtarLink = () => {
-  const input = document.querySelector("input") as HTMLInputElement;
-  const regex =  /\.com$/;
-  if (!regex.test(encodeURIComponent(input.value))) {
-    mensagemDminioCom();
-  }else if (input.value === '') {
-    mensagemVazioInput()
-  }else{
-    fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(input.value)}`)
-    .then(response => response.text())
-    .then(data => {
-      exibirUrl(data)
-      copiarUrl(data)
-    })
+const encurtarLink = async () => {
+  if (url.value === "") {
+    mensagemVazioInput();
+  } else {
+    try {
+      const data = await fetch("https://cleanuri.com/api/v1/shorten", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `url=${encodeURIComponent(url.value)}`,
+      });
+      const response = await data.json();
+      const {result_url} = response
+      exibirResultados(result_url)
+    } catch (error) {
+      console.error("Erro ao processar a solicitação:", error);
+    }
   }
 };
 
 botao.addEventListener("click", () => {
-  encurtarLink()
-})
+  encurtarLink();
+});
